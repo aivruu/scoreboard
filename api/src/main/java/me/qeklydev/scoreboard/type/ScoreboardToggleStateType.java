02a0ff5bd796ec36a003641a4a365f142d1140d4
Qekly <benjaalejandro206@gmail.com>
2024-04-13 18:52:11 -0400

    feat(state): Implement enum for toggle-state type representation for each scoreboard

---
+++ api/src/main/java/me/qeklydev/scoreboard/type/ScoreboardToggleStateType.java
@@ -0,0 +1,22 @@
+package me.qeklydev.scoreboard.type;
+
+/**
+ * This enum is used to represent the current toggled
+ * status for each scoreboard.
+ *
+ * @since 0.0.1
+ */
+public enum ScoreboardToggleStateType {
+  /**
+   * The scoreboard is visible for the player.
+   *
+   * @since 0.0.1
+   */
+  VISIBLE,
+  /**
+   * The scoreboard is closed for visibility.
+   *
+   * @since 0.0.1
+   */
+  CLOSED
+}
