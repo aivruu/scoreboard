267138a5cfd99fd07ed9ebda835336e32ba03860
Qekly <benjaalejandro206@gmail.com>
2024-04-14 22:10:08 -0400

    feat(docs): Create more documentation for some packages

---
+++ api/src/main/java/me/qeklydev/scoreboard/thread/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides an abstract model for handling with implementations
+ * that use an {@link java.util.concurrent.ScheduledExecutorService}.
+ *
+ * @since 0.0.1
+ */
+package me.qeklydev.scoreboard.thread;
---
+++ api/src/main/java/me/qeklydev/scoreboard/thread/result/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides results controller for shutdown operations
+ * with {@link me.qeklydev.scoreboard.thread.CustomExecutorThreadModel}.
+ *
+ * @since 0.0.1
+ */
+package me.qeklydev.scoreboard.thread.result;
