2cc5092f7e5e2cb82979d137cde6a5f475727cb7
Qekly <benjaalejandro206@gmail.com>
2024-04-18 21:53:13 -0400

    chore(title-updater): Implement methods for return and provide current title content list, and simplify one line the title updating

--- plugin/src/main/java/me/qeklydev/scoreboard/thread/impl/TitleUpdaterThreadModelImpl.java
+++ plugin/src/main/java/me/qeklydev/scoreboard/thread/impl/TitleUpdaterThreadModelImpl.java
@@ -1,18 +1,43 @@
 package me.qeklydev.scoreboard.thread.impl;
 
+import java.util.List;
 import java.util.concurrent.Executors;
 import me.qeklydev.scoreboard.repository.ScoreboardModelRepository;
 import me.qeklydev.scoreboard.thread.CustomExecutorThreadModel;
 import me.qeklydev.scoreboard.type.ScoreboardToggleStateType;
+import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
 public final class TitleUpdaterThreadModelImpl extends CustomExecutorThreadModel {
+  private List<Component> content;
   private byte index;
 
-  public TitleUpdaterThreadModelImpl(final @NotNull ScoreboardModelRepository scoreboardRepository) {
+  public TitleUpdaterThreadModelImpl(final @NotNull ScoreboardModelRepository scoreboardRepository,
+                                     final @NotNull List<@NotNull Component> content) {
     super(
         Executors.newSingleThreadScheduledExecutor(r -> new Thread(r, "ScoreboardTitleUpdaterExecutor")),
         scoreboardRepository);
+    this.content = content;
+  }
+
+  /**
+   * Sets a new list of components for this executor-model.
+   *
+   * @param newContent the new components list.
+   * @since 0.0.1
+   */
+  public void content(final @NotNull List<@NotNull Component> newContent) {
+    this.content = newContent;
+  }
+
+  /**
+   * Returns the list of components for this executor-model.
+   *
+   * @return The list of {@link Component} for this {@link CustomExecutorThreadModel}.
+   * @since 0.0.1
+   */
+  public @NotNull List<@NotNull Component> content() {
+    return this.content;
   }
 
   @Override
@@ -24,7 +49,7 @@ public final class TitleUpdaterThreadModelImpl extends CustomExecutorThreadModel
     if (++this.index == this.content.size()) {
       this.index = 0;
     }
-    for (final var scoreboardModel : this.scoreboardRepository.scoreboards()) {
+    for (final var scoreboardModel : super.scoreboardRepository.scoreboards()) {
       /*
        * If visibility status for the scoreboard is 'CLOSED',
        * skip this iteration.
@@ -32,8 +57,11 @@ public final class TitleUpdaterThreadModelImpl extends CustomExecutorThreadModel
       if (scoreboardModel.toggleState() == ScoreboardToggleStateType.CLOSED) {
         continue;
       }
-      final var scoreboardTitle = scoreboardModel.definedTitleForThisScoreboard();
-      scoreboardModel.sidebar().line(this.index, scoreboardTitle.get(this.index));
+      /*
+       * Establish the current content list index as the current animation
+       * line for the title.
+       */
+      scoreboardModel.updateTitle(this.content.get(this.index));
     }
   }
 }
