e430e307421813304a69fa1b5bbb6a46b10a0046
Qekly <benjaalejandro206@gmail.com>
2024-04-18 21:45:39 -0400

    chore(plugin): Modify some commentaries, include more parameters on constructors, and register scoreboard listener

--- plugin/src/main/java/me/qeklydev/scoreboard/ScoreboardPlugin.java
+++ plugin/src/main/java/me/qeklydev/scoreboard/ScoreboardPlugin.java
@@ -3,10 +3,13 @@ package me.qeklydev.scoreboard;
 import java.util.List;
 import me.qeklydev.scoreboard.config.Configuration;
 import me.qeklydev.scoreboard.config.ConfigurationProvider;
+import me.qeklydev.scoreboard.config.Messages;
+import me.qeklydev.scoreboard.listener.ScoreboardListener;
 import me.qeklydev.scoreboard.manager.ScoreboardManager;
 import me.qeklydev.scoreboard.repository.ScoreboardModelRepository;
 import me.qeklydev.scoreboard.thread.impl.ScoreboardUpdaterThreadModelImpl;
 import me.qeklydev.scoreboard.thread.impl.TitleUpdaterThreadModelImpl;
+import me.qeklydev.scoreboard.component.ComponentUtils;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +17,7 @@ import org.jetbrains.annotations.NotNull;
 public final class ScoreboardPlugin extends JavaPlugin implements ApiModel {
   private ComponentLogger logger;
   private ConfigurationProvider<Configuration> configProvider;
+  private ConfigurationProvider<Messages> messagesProvider;
   private ScoreboardModelRepository scoreboardRepository;
   private ScoreboardUpdaterThreadModelImpl scoreboardUpdaterThreadModel;
   private TitleUpdaterThreadModelImpl titleUpdaterThreadModel;
@@ -40,23 +44,29 @@ public final class ScoreboardPlugin extends JavaPlugin implements ApiModel {
     final var directory = super.getDataFolder().toPath();
     this.logger = super.getComponentLogger();
     this.configProvider = ConfigurationProvider.of(directory, "config", Configuration.class);
-    if (this.configProvider == null) {
-      throw new IllegalStateException("Configuration have not been loaded correctly.");
+    this.messagesProvider = ConfigurationProvider.of(directory, "messages", Messages.class);
+    /*
+     * Check if configurations was loaded correctly before
+     * continue with loading process.
+     */
+    if (this.configProvider == null || this.messagesProvider == null) {
+      this.logger.error("Configurations have not been loaded correctly, check them for any syntax error.");
     }
-    final var config = this.configProvider.get();
     this.scoreboardRepository = new ScoreboardModelRepository();
-    this.scoreboardManager = new ScoreboardManager(this.logger, this.scoreboardRepository);
-    this.scoreboardUpdaterThreadModel = new ScoreboardUpdaterThreadModelImpl(this.scoreboardRepository);
-    this.titleUpdaterThreadModel = new TitleUpdaterThreadModelImpl(this.scoreboardRepository);
+    this.scoreboardManager = new ScoreboardManager(this.logger, this.scoreboardRepository, this.configProvider);
+    this.scoreboardUpdaterThreadModel = new ScoreboardUpdaterThreadModelImpl(
+        this.scoreboardRepository, this.configProvider, this.logger);
+    this.titleUpdaterThreadModel = new TitleUpdaterThreadModelImpl(
+        this.scoreboardRepository, ComponentUtils.ofMany(this.configProvider.get().titleContent));
   }
 
   @Override
   public void onEnable() {
     /*
-     * If configuration have not been loaded correctly
+     * If configurations have not been loaded correctly
      * during on-load process, skip on-enable method execution.
      */
-    if (this.configProvider == null) {
+    if (this.configProvider == null || this.messagesProvider == null) {
       return;
     }
     ApiProvider.load(this);
@@ -65,9 +75,9 @@ public final class ScoreboardPlugin extends JavaPlugin implements ApiModel {
      * during loading, stop method execution.
      */
     if (!this.scoreboardManager.load(this)) {
-      this.logger.error("Something went wrong during scoreboard-manager start-up process.");
       return;
     }
+    super.getServer().getPluginManager().registerEvents(new ScoreboardListener(this.scoreboardManager, this.logger), this);
     final var config = this.configProvider.get();
     /*
      * Define update-rate values for the scoreboard content
