f57aa04e170fa45888bf9fa1e4ff22fcd6f7e583
Qekly <benjaalejandro206@gmail.com>
2024-04-18 21:36:39 -0400

    chore(component): Move ComponentUtils to 'component' package on 'plugin' subproject

--- plugin/src/main/java/me/qeklydev/scoreboard/component/ComponentUtils.java
+++ plugin/src/main/java/me/qeklydev/scoreboard/component/ComponentUtils.java
@@ -1,9 +1,10 @@
-package me.qeklydev.scoreboard.utils;
+package me.qeklydev.scoreboard.component;
 
 import java.util.ArrayList;
 import java.util.List;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.jetbrains.annotations.NotNull;
 
 public final class ComponentUtils {
@@ -11,6 +12,14 @@ public final class ComponentUtils {
 
   private ComponentUtils() {
     throw new UnsupportedOperationException("This class is for utility and cannot be instantiated.");
+  }
+
+  public static @NotNull Component ofSingle(final @NotNull String text) {
+    return MINI_MESSAGE.deserialize(text);
+  }
+
+  public static @NotNull Component ofSingleWith(final @NotNull String text, final TagResolver @NotNull... resolvers) {
+    return MINI_MESSAGE.deserialize(text, resolvers);
   }
 
   public static @NotNull List<@NotNull Component> ofMany(final @NotNull List<@NotNull String> text) {
