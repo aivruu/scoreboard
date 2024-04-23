31327dd6bbf66ce510bf6ba7d56057d921d6168e
Qekly <benjaalejandro206@gmail.com>
2024-04-23 16:57:41 -0400

    chore: Rename project and define subprojects name to ProBoard

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 @file:Suppress("UnstableApiUsage")
 
-rootProject.name = "Scoreboard"
+rootProject.name = "ProBoard"
 
 /*
  * Inclusion settings for common subprojects.
  */
 arrayOf("api", "plugin").forEach {
-    val project = ":scoreboard-$it"
+    val project = ":proboard-$it"
     include(project)
     project(project).projectDir = file(it)
 }
